d361a838e9e57462efffb588881dd42cd293eb97
Vlas <vpkabakov@gmail.com>
2023-08-21 17:45:51 +0300

    Fix 1.2.1

--- src/main/java/vlas/util/Frame.java
+++ src/main/java/vlas/util/Frame.java
@@ -5,12 +5,12 @@ import java.util.Objects;
 public class Frame {
     private String material;
     private String color;
-    private int holes_for_wheels;
+    private int holesForWheels;
 
-    public Frame(String material, String color, int holes_for_wheels) {
+    public Frame(String material, String color, int holesForWheels) {
         this.material = material;
         this.color = color;
-        this.holes_for_wheels = holes_for_wheels;
+        this.holesForWheels = holesForWheels;
     }
     public Frame(){}
 
@@ -31,11 +31,11 @@ public class Frame {
     }
 
     public int getHolesForWheels() {
-        return holes_for_wheels;
+        return holesForWheels;
     }
 
     public void setHolesForWheels(int holes_for_wheels) {
-        this.holes_for_wheels = holes_for_wheels;
+        this.holesForWheels = holes_for_wheels;
     }
 
     @Override
@@ -43,12 +43,12 @@ public class Frame {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Frame frame = (Frame) o;
-        return holes_for_wheels == frame.holes_for_wheels && Objects.equals(material, frame.material) && Objects.equals(color, frame.color);
+        return holesForWheels == frame.holesForWheels && Objects.equals(material, frame.material) && Objects.equals(color, frame.color);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(material, color, holes_for_wheels);
+        return Objects.hash(material, color, holesForWheels);
     }
 
     @Override
@@ -56,7 +56,7 @@ public class Frame {
         return "Frame{" +
                 "material='" + material + '\'' +
                 ", color='" + color + '\'' +
-                ", holes_for_wheels=" + holes_for_wheels +
+                ", holes_for_wheels=" + holesForWheels +
                 '}';
     }
 }
